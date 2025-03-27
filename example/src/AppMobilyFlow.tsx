e677b01ddb0e0d13ef2c1182876c3b5591a87037
LaGregance <gregoire.taja@gmail.com>
2025-03-27 20:56:36 +0100

    Add IAPHub testing

---
+++ example/src/AppMobilyFlow.tsx
@@ -0,0 +1,125 @@
+import { Button, Platform, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { MobilyPurchaseSDK } from 'mobilyflow-react-native-sdk';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import type { MobilyProduct } from '../../src/entities/mobily-product';
+import { ProductButton } from './ProductButton';
+import type { MobilySubscriptionOffer } from '../../src/entities/mobily-subscription-offer';
+import { MobilyEnvironment } from '../../src/enums/mobily-environment';
+import { MobilyPurchaseError } from '../../src/errors/mobily-purchase-error';
+
+export default function AppMobilyFlow() {
+  const [products, setProducts] = useState<MobilyProduct[]>();
+  const [storeCountry, setStoreCountry] = useState<string>();
+  const [error, setError] = useState('');
+
+  const sdk = useRef<MobilyPurchaseSDK>();
+  const firstTime = useRef(true);
+
+  const init = useCallback(async () => {
+    sdk.current = new MobilyPurchaseSDK(
+      'caecc000-45ce-49b3-b218-46c1d985ae85',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYXBwLXRva2VuIiwic3ViIjoiY2FlY2MwMDAtNDVjZS00OWIzLWIyMTgtNDZjMWQ5ODVhZTg1Iiwic2NvcGUiOjEwLCJpYXQiOjE3MzczNTYyNzIsImV4cCI6MzMyOTQ5NTYyNzJ9.2GDcRmX2dJEfN3S4HANygmOwXqSyGOIsTXVHu5LrLtc',
+      MobilyEnvironment.DEVELOPMENT,
+      {
+        // languages: ['en', 'fr'],
+        apiURL: 'https://api-staging.mobilyflow.com/v1/',
+        debug: true,
+      }
+    );
+
+    try {
+      await sdk.current.login('914b9a20-950b-44f7-bd7b-d81d57992294'); // gregoire
+      const p = await sdk.current.getProducts();
+      console.log('Products: ', p);
+      setProducts(p);
+      setStoreCountry(Platform.OS === 'ios' ? await sdk.current.getStoreCountry() : '-');
+      console.log('Done');
+    } catch (e: any) {
+      setError(`Error: ${e.code} ${e.domain}`);
+      console.error('Error: ', e.code, e.domain);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (firstTime.current) {
+      firstTime.current = false;
+      init().then();
+    }
+  }, [init]);
+
+  const handlePurchase = async (product: MobilyProduct, offer?: MobilySubscriptionOffer) => {
+    try {
+      console.log(`Click ${product.identifier} ${offer?.ios_offerId}`);
+      const result = await sdk.current.purchaseProduct(product, { offer });
+      console.log('Purchase result = ', result);
+    } catch (e: any) {
+      if (e instanceof MobilyPurchaseError) {
+        console.error('Purchase error: ', e.type);
+      } else {
+        console.error('Purchase error: ', e);
+      }
+    }
+  };
+
+  const handleRefresh = async () => {
+    await init();
+  };
+
+  const handleManageSubscriptions = async () => {
+    await sdk.current.openManageSubscription();
+  };
+
+  const handleTransferOwnership = async () => {
+    await sdk.current.requestTransferOwnership();
+  };
+
+  const handleSendDiagnostic = () => {
+    sdk.current.sendDiagnotic();
+  };
+
+  const handleRefundRequest = () => {
+    // sdk.re
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>MobilyFlow React Native</Text>
+      <Text>{storeCountry}</Text>
+      {error && <Text style={{ color: 'red' }}>{error}</Text>}
+
+      <ScrollView style={{ alignSelf: 'stretch', marginTop: 20 }}>
+        <View style={{ alignItems: 'stretch', alignSelf: 'stretch', gap: 10, padding: 10 }}>
+          {products?.map((product) => (
+            <View key={product.id} style={{ gap: 10 }}>
+              <ProductButton product={product} handlePress={handlePurchase} />
+              {product?.subscriptionProduct?.promotionalOffers?.length > 0 && (
+                <View style={{ gap: 10, paddingHorizontal: 10 }}>
+                  {product?.subscriptionProduct?.promotionalOffers?.map((offer) => (
+                    <ProductButton key={offer.id} product={product} offer={offer} handlePress={handlePurchase} />
+                  ))}
+                </View>
+              )}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+      <View style={{ marginTop: 20, alignSelf: 'stretch', gap: 5 }}>
+        <Button title="Refresh" onPress={handleRefresh} />
+        <Button title="Manage subscriptions" onPress={handleManageSubscriptions} />
+        <Button title="Transfer Ownership" onPress={handleTransferOwnership} />
+        <Button title="Send diagnostic" onPress={handleSendDiagnostic} />
+        <Button title="Refund Request" onPress={handleRefundRequest} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+  },
+});
---
+++ example/src/iaphub/AppIapHub.tsx
@@ -0,0 +1,90 @@
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Iaphub from 'react-native-iaphub';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { MobilyPurchaseError } from '../../../src/errors/mobily-purchase-error';
+import type Product from 'react-native-iaphub/lib/typescript/models/product';
+import { IapHubProductButton } from './IapHubProductButton';
+
+export default function AppIapHub() {
+  const [products, setProducts] = useState<Product[]>();
+  const [error, setError] = useState('');
+
+  const firstTime = useRef(true);
+
+  const init = useCallback(async () => {
+    try {
+      await Iaphub.start({
+        appId: '645cda0beb93424510aa4bb2',
+        apiKey: 'aZo1MaZ19J93WMvFU2DlAfypTxwtabPh',
+        enableStorekitV2: true,
+        environment: 'staging',
+        lang: 'en',
+        userId: '914b9a20-950b-44f7-bd7b-d81d57992294', // gregoire
+      });
+
+      const p = await Iaphub.getProductsForSale();
+      setProducts(p);
+    } catch (err) {
+      setError(`${err}`);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (firstTime.current) {
+      firstTime.current = false;
+      init().then();
+    }
+  }, [init]);
+
+  const handlePurchase = async (product: Product) => {
+    try {
+      const startTime = Date.now();
+      console.log(`Click ${product.id} ${product.sku}`);
+      const result = await Iaphub.buy(product.sku);
+      console.log(`Purchase result (${Date.now() - startTime}ms) = `, result);
+    } catch (e: any) {
+      if (e instanceof MobilyPurchaseError) {
+        console.error('Purchase error: ', e.type);
+      } else {
+        console.error('Purchase error: ', e);
+      }
+    }
+  };
+
+  const handleRefresh = async () => {
+    await init();
+  };
+
+  const handleManageSubscriptions = async () => {
+    await Iaphub.showManageSubscriptions();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>IAPHub React Native</Text>
+      {error && <Text style={{ color: 'red' }}>{error}</Text>}
+
+      <ScrollView style={{ alignSelf: 'stretch', marginTop: 20 }}>
+        <View style={{ alignItems: 'stretch', alignSelf: 'stretch', gap: 10, padding: 10 }}>
+          {products?.map((product) => (
+            <IapHubProductButton key={product.id} product={product} handlePress={handlePurchase} />
+          ))}
+          <View style={{ marginTop: 20, alignSelf: 'stretch', gap: 5 }}>
+            <Button title="Refresh" onPress={handleRefresh} />
+            <Button title="Manage subscriptions" onPress={handleManageSubscriptions} />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+  },
+});

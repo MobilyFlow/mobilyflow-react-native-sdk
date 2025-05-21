307bcd7cfc239ced0f8a5f117231e9360fe85ed5
LaGregance <gregoire.taja@gmail.com>
2025-05-21 11:53:51 +0200

    0.1.1-alpha.19

--- src/NativeMobilyflowReactNativeSdk.ts
+++ src/NativeMobilyflowReactNativeSdk.ts
@@ -5,6 +5,7 @@ import { MobilySubscriptionGroup } from './entities/mobily-subscription-group';
 import { MobilyCustomerEntitlement } from './entities/mobily-customer-entitlement';
 import { WebhookStatus } from './enums/webhook-status';
 import type { MobilyCustomer } from './entities/mobily-customer';
+import { TransferOwnershipStatus } from './enums/transfer-ownership-status';
 
 export type MobilyPurchaseSDKOptions = {
   languages?: string[];
@@ -33,7 +34,7 @@ export interface Spec extends TurboModule {
   getEntitlement(uuid: string, productId: string): Promise<MobilyCustomerEntitlement | null>;
   getEntitlements(uuid: string, productIds?: string[]): Promise<MobilyCustomerEntitlement[]>;
 
-  requestTransferOwnership(uuid: string): Promise<void>;
+  requestTransferOwnership(uuid: string): Promise<TransferOwnershipStatus>;
   openManageSubscription(uuid: string): Promise<void>;
   openRefundDialog(uuid: string, productId: string): Promise<boolean>;
 
---
+++ src/enums/transfer-ownership-status.ts
@@ -0,0 +1,7 @@
+export enum TransferOwnershipStatus {
+  PENDING,
+  ERROR,
+  DELAYED,
+  ACKNOWLEDGED,
+  REJECTED,
+}
